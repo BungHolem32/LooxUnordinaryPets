a3aab2e696a5d9330b73e48f653cb120f71790ec
Ilan Vachtel <ilanvac@gamil.com>
2019-04-23 23:39:07 +0300

    server: - fix endpoint http://localhost:3000/pets/:id - redirect index page to pets endpoint - return readable response to user
    client:
    - fix linting

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -4,14 +4,23 @@ var petsList = require('../data/pets.json');
 
 /* GET Pets List. */
 router.get('/', function(req, res, next) {
-  res.json(petsList);
+  let responseMessage = {data:null,status:res.statusCode, message: `no records found`};
+  if(petsList){
+    responseMessage = Object.assign(responseMessage,{data:petsList,message:'records return successfully'})
+  }
+  res.json(responseMessage);
 });
 
 /* GET Specific pet. */
 router.get('/:id', function(req, res, next) {
   let id = req.params.id;
+  let responseMessage = {data:null,status:res.statusCode,message: `no record with the id ${id} found`};
   let pet = petsList.filter((pet) => pet.id === id);
-  res.json(pet[0]);
+
+  if(pet.length){
+    responseMessage = Object.assign(responseMessage,{data:pet[0],message:`record with the id ${id} return successfully`});
+  }
+  res.json(responseMessage);
 });
 
 // Another option displaying in jade template
