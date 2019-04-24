959de1e397df6efc7c37d065f01172155182977f
Ilan Vachtel <ilanvac@gamil.com>
2019-04-24 06:33:17 +0300

    server: - minify it to only one file.

--- client/test/e2e/runner.js
+++ client/test/e2e/runner.js
@@ -1,4 +1,4 @@
-// 1. start the dev server using production config
+// 1. start the dev server3 using production config
 process.env.NODE_ENV = 'testing'
 
 const webpack = require('webpack')
--- server/app.js
+++ server/app.js
@@ -1,44 +1,33 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var petsRouter = require("./routes/pets");
-var cors = require("cors");
+var express = require('express');
+var cors = require('cors');
+var petsList = require('./data/pets.json');
 
 var app = express();
-
+var port = 3000;
 app.use(cors());
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/pets", petsRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/',(req,res) => {
+  res.redirect('/pets');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.get('/pets',(req,res) => {
+  let responseObject = {data:null,status:res.statusCode, message: `no records found`};
+  if(petsList){
+    responseObject = Object.assign(responseObject,{data:petsList,message:'records return successfully'})
+  }
+  res.json(responseObject);
 });
 
-module.exports = app;
+app.get('/pets/:id',(req,res) => {
+  let id = req.params.id;
+  let responseObject = {data:null,status:res.statusCode,message: `no record with the id ${id} found`};
+  let pet = petsList.filter((pet) => pet.id === id);
+
+  if(pet.length){
+    responseObject = Object.assign(responseObject,{data:pet[0],message:`record with the id ${id} return successfully`});
+  }
+  res.json(responseObject);
+});
+
+app.listen(port, () => console.log(`Pets api is listening on port ${port}`));
